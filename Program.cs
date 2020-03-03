d085034bb3f21fd7c89c6236e02eac8b930520d9
FABRICIO <fnmamao@gmail.com>
2020-03-02 21:34:27 -0300

    adiciona imagem

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace exercicio_arte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\" ||  ||");
+            Console.WriteLine(" \\\\()//");
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+        }
+    }
+}
